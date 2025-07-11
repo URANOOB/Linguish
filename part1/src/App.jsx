23e571df8e14e52eea5c489ee0ad472443423e3f
William Galeano <will21@hotmail.es>
2025-07-10 21:36:31 -0500

    First part of the exercise

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,21 +1,29 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises} />
+      <Total exercises={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
 }
 
 const Header = (props) => {
+  console.log(props)
   return (
     <div>
       <h1>{props.course}</h1>
@@ -24,6 +32,7 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props)
   return (
     <div>
       <p>{props.part1} {props.exercises1}</p>
@@ -34,16 +43,13 @@ const Content = (props) => {
 }
 
 const Total = (props) =>{
-  console.log(props.exercises1, props.exercises2, props.exercises3)
-  if (props.exercises1 + props.exercises2 + props.exercises3 != 31) {
-    return console.log("Good luck")
-  } else {
+  console.log(props)
     return (
       <div>
-        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+        <p>Number of exercises: {props.exercises}</p>
       </div>
     )
-  }
 }
+
 
 export default App
