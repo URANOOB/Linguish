a2ee14aac90426d05707cd090121496ebdc2fb10
URANOOB <will21@hotmail.es>
2025-07-21 20:34:20 -0500

    Undoing some stuff

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,51 +5,23 @@ const Hello = (props) => {
     return theYear
   }
 
-  const birthdayMonth = () => {
-    const months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December'
-    ]
-    const monthName = months[props.monthBirth - 1]
-    return monthName
-  }
-
-  const actualDate = () => {
-
-    const year = bornYear()
-    const numberOfDays = new Date(year, props.monthBirth, 0).getDate()
-    return numberOfDays
-  }
-
   return (
     <div>
       <p>Hello {props.name}, you are {props.age} years old</p>
-      <p>So you were probably born in {bornYear()}, in the month of {birthdayMonth()}</p>
-      <p>The days of the month were {actualDate()}</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
 
 const App = () => {
   const name = 'Peter'
-  const age = 25
-  const monthBirth = 12
+  const age = 10
 
   return (
     <div>
       <h1>Greetings</h1>
-
-      <Hello name={name} age={age} monthBirth={monthBirth} />
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
